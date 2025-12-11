296213fef8f06ff1609646edc9def97429176840
Hafizh Habiibi Lubis <hafizhhabiibi123@gmail.com>
2025-12-12 01:19:34 +0700

    feat(models): menambahkan model komentar

---
+++ laravel_passport_api/app/Models/Komentar.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Komentar extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'berita_id',
+        'isi_komentar',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function berita()
+    {
+        return $this->belongsTo(Berita::class);
+    }
+}
