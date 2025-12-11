d88e5de41090b7863af132bf4875f57b24944730
Hafizh Habiibi Lubis <hafizhhabiibi123@gmail.com>
2025-12-12 01:20:21 +0700

    feat(api): menambahkan route berita dan komentar

--- laravel_passport_api/routes/api.php
+++ laravel_passport_api/routes/api.php
@@ -1,15 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthenticationController;
+use App\Http\Controllers\API\BeritaController;
+use App\Http\Controllers\API\KomentarController;
 
-Route::group(['namespace' => 'App\Http\Controllers\API'], function () {
-    // --------------- Register and Login ----------------//
-    Route::post('register', 'AuthenticationController@register')->name('register');
-    Route::post('login', 'AuthenticationController@login')->name('login');
-    
-    // ------------------ Get Data ----------------------//
-    Route::middleware('auth:api')->group(function () {
-        Route::get('get-user', 'AuthenticationController@userInfo')->name('get-user');
-        Route::post('logout', 'AuthenticationController@logOut')->name('logout');
-    });
+// ----------- Public Routes (tanpa login) -------------- //
+Route::post('/register', [AuthenticationController::class, 'register']);
+Route::post('/login', [AuthenticationController::class, 'login']);
+
+// ------------- Protected Routes (butuh token) --------- //
+Route::middleware('auth:api')->group(function () {
+
+    // Auth
+    Route::get('/get-user', [AuthenticationController::class, 'userInfo']);
+    Route::post('/logout', [AuthenticationController::class, 'logOut']);
+
+    // ---------------- CRUD BERITA ---------------- //
+    Route::get('/berita', [BeritaController::class, 'index']);
+    Route::post('/berita', [BeritaController::class, 'store']);
+    Route::get('/berita/{id}', [BeritaController::class, 'show']);
+    Route::put('/berita/{id}', [BeritaController::class, 'update']);
+    Route::delete('/berita/{id}', [BeritaController::class, 'destroy']);
+
+    // ---------------- KOMENTAR ---------------- //
+    Route::post('/komentar', [KomentarController::class, 'store']);
+    Route::delete('/komentar/{id}', [KomentarController::class, 'destroy']);
 });
